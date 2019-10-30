b8c612faf61dc16ef21e8b7753020652d2d7be7d
Huang-Yujie <huangantimony@gmail.com>
2019-10-30 23:35:51 +0800

    update

--- iMusic/View/PlayerView.swift
+++ iMusic/View/PlayerView.swift
@@ -11,9 +11,78 @@ import UIKit
 class PlayerView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setUpViews()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    private func setUpViews() {
+        self.addSubview(image)
+//        self.addSubview(songNameLabel)
+//        self.addSubview(progressSlider)
+//        self.addSubview(backwardButton)
+//        self.addSubview(forwardButton)
+//        self.addSubview(playPauseButton)
+//        self.addSubview(volumeSlider)
+        
+//        image.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor).isActive = true
+//        image.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor).isActive = true
+//        image.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor).isActive = true
+        image.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        image.heightAnchor.constraint(equalToConstant: 200).isActive = true       
+        
+    }
+    
+    let image: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named:"Galway Girl" )
+//        imageView.largeContentImageInsets = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let songNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Galway Gril"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let progressSlider: UISlider = {
+        let slider = UISlider()
+//        slider.
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        return slider
+    }()
+    
+    let backwardButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "backward.fill"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let forwardButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "forward.fill"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let playPauseButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let volumeSlider: UISlider = {
+        let slider = UISlider()
+        slider.minimumValueImage = UIImage(systemName: "speaker.fill")
+        slider.maximumValueImage = UIImage(systemName: "speaker.3.fill")
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        return slider
+    }()
+    
 }
